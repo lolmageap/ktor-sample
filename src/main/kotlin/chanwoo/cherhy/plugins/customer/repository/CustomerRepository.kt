730976f92912ca2899d873b6f24ca8b9ca02a0a1
lolmageap <ekxk1234@naver.com>
2024-04-20 22:24:17 +0900

    fix business logic to exposed

--- src/main/kotlin/chanwoo/cherhy/plugins/customer/repository/CustomerRepository.kt
+++ src/main/kotlin/chanwoo/cherhy/plugins/customer/repository/CustomerRepository.kt
@@ -1,11 +1,39 @@
 package chanwoo.cherhy.plugins.customer.repository
 
+import chanwoo.cherhy.plugins.customer.entity.Customer
+import chanwoo.cherhy.plugins.customer.entity.Customers
+import chanwoo.cherhy.plugins.customer.request.CustomerRequest
+import chanwoo.cherhy.plugins.customer.response.CustomerResponse
+import chanwoo.cherhy.plugins.util.PageRequest
+import org.jetbrains.exposed.sql.selectAll
 
-object CustomerRepository {
+interface CustomerRepository {
+    fun save(request: CustomerRequest, encodedPassword: String)
+    fun getAll(request: PageRequest): List<CustomerResponse>
+    fun get(id: Long): CustomerResponse
+}
 
-
-    fun findAll(){
-
+object CustomerRepositoryImpl: CustomerRepository {
+    override fun save(
+        request: CustomerRequest,
+        encodedPassword: String,
+    ) {
+        Customer.new {
+            name = request.name
+            email = request.email
+            password = encodedPassword
+            phoneNumber = request.phoneNumber
+        }
     }
 
+    override fun getAll(request: PageRequest) =
+        Customers.selectAll()
+            .limit(request.size, request.offset)
+            .map(Customer::wrapRow)
+            .map(CustomerResponse::of)
+
+    override fun get(id: Long) =
+        Customer.findById(id)
+            ?.let(CustomerResponse::of)
+            ?: throw IllegalArgumentException("Customer not found")
 }
