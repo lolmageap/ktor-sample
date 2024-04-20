128e9c356537fae2b119142d0322b3f146db64c4
lolmageap <ekxk1234@naver.com>
2024-04-20 22:21:07 +0900

    add exposed, koin, postgres dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,9 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-
 plugins {
     id("io.ktor.plugin") version "2.3.3"
     kotlin("jvm") version "1.9.0"
-    kotlin("plugin.jpa") version "1.7.22"
 }
+
+val exposedVersion = "0.49.0"
 
 group = "com.example"
 version = "0.0.1"
@@ -23,25 +20,32 @@ repositories {
 }
 
 dependencies {
-
     implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-auth-jvm")
-    implementation("io.ktor:ktor-server-auth-jwt-jvm")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-server-config-yaml")
+    implementation("io.ktor:ktor-serialization-jackson-jvm")
+    implementation("io.ktor:ktor-server-call-logging-jvm")
     implementation("io.ktor:ktor-server-swagger-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
-
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("io.ktor:ktor-serialization-jackson:$ktor_version")
-    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
-    implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
-    implementation("org.hibernate:hibernate-core:6.1.7.Final")
-    implementation("org.hibernate:hibernate-entitymanager:5.6.15.Final")
-    implementation("io.github.microutils:kotlin-logging:3.0.5")
-    implementation(group = "com.linecorp.kotlin-jdsl", name = "spring-data-kotlin-jdsl-starter", version = "2.0.4.RELEASE")
-
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation(group = "org.postgresql", name = "postgresql", version = "42.1.4")
+    implementation("io.ktor:ktor-server-websockets-jvm:2.3.2")
     testImplementation("io.ktor:ktor-server-tests-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    implementation("io.ktor:ktor-server-status-pages")
 
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-json:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-money:$exposedVersion")
+
+    implementation("org.postgresql:postgresql:42.7.3")
+    implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("io.ktor:ktor-server-websockets")
+
+    implementation("io.insert-koin:koin-ktor:3.5.6")
+
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
