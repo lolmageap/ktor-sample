7e9efd552e5e091ca6f3c602f5b3cf0de1ab0e86
lolmageap <ekxk1234@naver.com>
2024-04-22 21:49:13 +0900

    create crud example

--- src/main/kotlin/chanwoo/cherhy/plugins/domain/customer/CustomerRepository.kt
+++ src/main/kotlin/chanwoo/cherhy/plugins/domain/customer/CustomerRepository.kt
@@ -7,8 +7,10 @@ import org.jetbrains.exposed.sql.selectAll
 
 interface CustomerRepository {
     fun save(request: CustomerRequest, encodedPassword: String)
-    fun getAll(request: PageRequest): List<CustomerResponse>
-    fun get(id: Long): CustomerResponse
+    fun findAll(request: PageRequest): List<CustomerResponse>
+    fun findById(id: Long): CustomerResponse
+    fun update(id: Long, request: CustomerRequest)
+    fun delete(id: Long)
 }
 
 class CustomerRepositoryImpl: CustomerRepository {
@@ -24,14 +26,26 @@ class CustomerRepositoryImpl: CustomerRepository {
         }
     }
 
-    override fun getAll(request: PageRequest) =
+    override fun findAll(request: PageRequest) =
         Customers.selectAll()
             .limit(request.size, request.offset)
             .map(Customer::wrapRow)
             .map(CustomerResponse::of)
 
-    override fun get(id: Long) =
+    override fun findById(id: Long) =
         Customer.findById(id)
             ?.let(CustomerResponse::of)
             ?: throw IllegalArgumentException("Customer not found")
+
+    override fun update(id: Long, request: CustomerRequest) {
+        Customer.findById(id)?.apply {
+            name = request.name
+            email = request.email
+            phoneNumber = request.phoneNumber
+        } ?: throw IllegalArgumentException("Customer not found")
+    }
+
+    override fun delete(id: Long) {
+        Customer.findById(id)?.delete()
+    }
 }
--- src/main/kotlin/chanwoo/cherhy/plugins/domain/customer/CustomerService.kt
+++ src/main/kotlin/chanwoo/cherhy/plugins/domain/customer/CustomerService.kt
@@ -14,9 +14,11 @@ class CustomerService(
             customerRepository.save(request, it)
         }
 
-    fun getAll(request: PageRequest) =
-        customerRepository.getAll(request)
+    fun getAll(request: PageRequest) = customerRepository.findAll(request)
 
-    fun get(id: Long) =
-        customerRepository.get(id)
+    fun get(id: Long) = customerRepository.findById(id)
+
+    fun update(id: Long, request: CustomerRequest) = customerRepository.update(id, request)
+
+    fun delete(id: Long) = customerRepository.delete(id)
 }
