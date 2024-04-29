b9910bb555b159ca521500f5f960227569db50d6
lolmageap <ekxk1234@naver.com>
2024-04-29 22:31:53 +0900

    fix to data source setting

--- src/main/kotlin/chanwoo/cherhy/plugins/config/DatabaseConfig.kt
+++ src/main/kotlin/chanwoo/cherhy/plugins/config/DatabaseConfig.kt
@@ -4,17 +4,18 @@ import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import io.ktor.server.application.*
 import org.jetbrains.exposed.sql.Database
+import chanwoo.cherhy.plugins.util.ApplicationConfigUtils.getDataSource
 
 fun Application.configureDatabase() {
     val hikari = HikariDataSource(
         HikariConfig().apply {
-            jdbcUrl = environment.config.property("ktor.database.url").getString()
-            username = environment.config.property("ktor.database.username").getString()
-            password = environment.config.property("ktor.database.password").getString()
-            driverClassName = environment.config.property("ktor.database.driverClassName").getString()
-            maximumPoolSize = environment.config.property("ktor.database.maxPoolSize").getString().toInt()
+            jdbcUrl = getDataSource("url")
+            username = getDataSource("username")
+            password = getDataSource("password")
+            driverClassName = getDataSource("driver-class-name")
+            maximumPoolSize = getDataSource("max-pool-size").toInt()
             isAutoCommit = false
-            transactionIsolation = environment.config.property("ktor.database.isolationLevel").getString()
+            transactionIsolation = getDataSource("isolation-level")
             validate()
         }
     )
