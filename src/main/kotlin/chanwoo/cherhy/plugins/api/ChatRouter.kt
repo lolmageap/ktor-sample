028daebf85b6cb9d9d1fed5a46f5b503ed5adbd9
lolmageap <ekxk1234@naver.com>
2024-04-29 22:31:28 +0900

    refactor to web socket router

--- src/main/kotlin/chanwoo/cherhy/plugins/api/ChatRouter.kt
+++ src/main/kotlin/chanwoo/cherhy/plugins/api/ChatRouter.kt
@@ -1,20 +1,29 @@
 package chanwoo.cherhy.plugins.api
 
+import chanwoo.cherhy.plugins.domain.chat.Connection
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
+import java.util.Collections
+
+private val connectionFactory = Collections.synchronizedSet(HashSet<Connection>())
 
 fun Route.chat() {
     webSocket("/echo") {
+        connectionFactory + Connection(this)
         send("Please enter your name")
-        for (frame in incoming) {
-            frame as? Frame.Text ?: continue
-            val receivedText = frame.readText()
-            if (receivedText.equals("bye", ignoreCase = true)) {
-                close(CloseReason(CloseReason.Codes.NORMAL, "Client said BYE"))
-            } else {
-                send(Frame.Text("Hi, $receivedText!"))
+
+        try {
+            for (frame in incoming) {
+                frame as? Frame.Text ?: continue
+                val receivedText = frame.readText()
+                val message = "${connectionFactory.find { it.session == this }?.name}: $receivedText"
+                connectionFactory.forEach { it.session.send(message) }
             }
+        } catch (e: Exception) {
+            println(e.localizedMessage)
+        } finally {
+            connectionFactory - Connection(this)
         }
     }
 }
