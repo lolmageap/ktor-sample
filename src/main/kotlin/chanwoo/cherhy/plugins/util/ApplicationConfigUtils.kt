d7f0e50103578b912f86bf4155ea8a8edd15ebd6
lolmageap <ekxk1234@naver.com>
2024-04-29 22:31:05 +0900

    create application configuration utils

---
+++ src/main/kotlin/chanwoo/cherhy/plugins/util/ApplicationConfigUtils.kt
@@ -0,0 +1,11 @@
+package chanwoo.cherhy.plugins.util
+
+import com.typesafe.config.ConfigFactory
+import io.ktor.server.config.*
+
+object ApplicationConfigUtils {
+    private fun getConfigProperty(path: String) =
+        HoconApplicationConfig(ConfigFactory.load()).property(path).getString()
+
+    fun getDataSource(key: String) = getConfigProperty("database.datasource.$key")
+}
