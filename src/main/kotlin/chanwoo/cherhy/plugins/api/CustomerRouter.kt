74e3824b71012984a96bea74e34eef280d183526
lolmageap <ekxk1234@naver.com>
2024-04-30 23:53:50 +0900

    add authenticate security scope

--- src/main/kotlin/chanwoo/cherhy/plugins/api/CustomerRouter.kt
+++ src/main/kotlin/chanwoo/cherhy/plugins/api/CustomerRouter.kt
@@ -1,14 +1,18 @@
 package chanwoo.cherhy.plugins.api
 
+import chanwoo.cherhy.plugins.domain.customer.CustomerService
 import chanwoo.cherhy.plugins.util.EndPoint.CUSTOMER.CREATE_CUSTOMER
+import chanwoo.cherhy.plugins.util.EndPoint.CUSTOMER.DELETE_CUSTOMER
 import chanwoo.cherhy.plugins.util.EndPoint.CUSTOMER.GET_CUSTOMER
 import chanwoo.cherhy.plugins.util.EndPoint.CUSTOMER.GET_CUSTOMERS
-import chanwoo.cherhy.plugins.domain.customer.CustomerService
-import chanwoo.cherhy.plugins.util.EndPoint.CUSTOMER.DELETE_CUSTOMER
+import chanwoo.cherhy.plugins.util.EndPoint.CUSTOMER.LOGIN
 import chanwoo.cherhy.plugins.util.EndPoint.CUSTOMER.UPDATE_CUSTOMER
+import chanwoo.cherhy.plugins.util.JwtManager
 import chanwoo.cherhy.plugins.util.PageRequest
+import chanwoo.cherhy.plugins.util.SecurityProperty.AUTHORITY
 import chanwoo.cherhy.plugins.util.id
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -16,34 +20,54 @@ import org.koin.ktor.ext.inject
 
 fun Route.customer() {
     val customerService: CustomerService by inject()
-    
-    get(GET_CUSTOMERS) {
-        val requestCondition = call.receive<PageRequest>()
-        customerService.getAll(requestCondition)
+    val jwtManager: JwtManager by inject()
+
+    authenticate(AUTHORITY) {
+        get(LOGIN) {
+            val request = call.receive<LoginRequest>()
+            // TODO: insert validation logic
+            val jwt = jwtManager.createToken(request.username)
+            call.respond(jwt)
+        }
     }
 
-    get(GET_CUSTOMER) {
-        val customerId = call.id
-        val customer = customerService.get(customerId)
-        call.respond(customer)
+    authenticate(AUTHORITY) {
+        get(GET_CUSTOMERS) {
+            val request = call.receive<PageRequest>()
+            customerService.getAll(request)
+        }
     }
 
-    post(CREATE_CUSTOMER) {
-        val request = call.receive<CustomerRequest>()
-        val customer = customerService.save(request)
-        call.respond(customer)
+    authenticate(AUTHORITY) {
+        get(GET_CUSTOMER) {
+            val customerId = call.id
+            val customer = customerService.get(customerId)
+            call.respond(customer)
+        }
     }
 
-    put(UPDATE_CUSTOMER) {
-        val customerId = call.id
-        val request = call.receive<CustomerRequest>()
-        customerService.update(customerId, request)
-        call.respond("Customer updated")
+    authenticate(AUTHORITY) {
+        post(CREATE_CUSTOMER) {
+            val request = call.receive<CustomerRequest>()
+            val customer = customerService.save(request)
+            call.respond(customer)
+        }
     }
 
-    delete(DELETE_CUSTOMER) {
-        val customerId = call.id
-        customerService.delete(customerId)
-        call.respond("Customer deleted")
+    authenticate(AUTHORITY) {
+        put(UPDATE_CUSTOMER) {
+            val customerId = call.id
+            val request = call.receive<CustomerRequest>()
+            customerService.update(customerId, request)
+            call.respond("Customer updated")
+        }
+    }
+
+    authenticate(AUTHORITY) {
+        delete(DELETE_CUSTOMER) {
+            val customerId = call.id
+            customerService.delete(customerId)
+            call.respond("Customer deleted")
+        }
     }
 }
