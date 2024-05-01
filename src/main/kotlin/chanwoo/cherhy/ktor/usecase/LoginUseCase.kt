a0ad6459a24d25896ac95a836b90daca8cc57fa3
lolmageap <ekxk1234@naver.com>
2024-05-01 17:10:59 +0900

    create login use case

---
+++ src/main/kotlin/chanwoo/cherhy/ktor/usecase/LoginUseCase.kt
@@ -0,0 +1,28 @@
+package chanwoo.cherhy.ktor.usecase
+
+import chanwoo.cherhy.ktor.api.CustomerRequest
+import chanwoo.cherhy.ktor.api.LoginRequest
+import chanwoo.cherhy.ktor.domain.customer.CustomerService
+import chanwoo.cherhy.ktor.util.JwtManager
+import chanwoo.cherhy.ktor.util.matches
+import java.util.*
+
+class LoginUseCase(
+    private val customerService: CustomerService,
+    private val jwtManager: JwtManager,
+    private val passwordEncoder: Base64.Encoder,
+) {
+    fun execute(request: LoginRequest): String {
+        val customer = customerService.get(request.email)
+        passwordEncoder.matches(request.password, customer.password)
+
+        return jwtManager.createToken(customer.id, request.email)
+    }
+
+    fun execute(request: CustomerRequest): String {
+        val customer = customerService.get(request.email)
+        passwordEncoder.matches(request.password, customer.password)
+
+        return jwtManager.createToken(customer.id, request.email)
+    }
+}
