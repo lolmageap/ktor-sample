dc7c4f2ef0a8ed2869546c5c8d8f3b7306654a6d
lolmageap <ekxk1234@naver.com>
2024-05-01 17:12:59 +0900

    add login and signup and remove unused api

--- src/main/kotlin/chanwoo/cherhy/ktor/api/CustomerRouter.kt
+++ src/main/kotlin/chanwoo/cherhy/ktor/api/CustomerRouter.kt
@@ -1,16 +1,16 @@
 package chanwoo.cherhy.ktor.api
 
 import chanwoo.cherhy.ktor.domain.customer.CustomerService
-import chanwoo.cherhy.ktor.util.EndPoint.CUSTOMER.CREATE_CUSTOMER
+import chanwoo.cherhy.ktor.usecase.LoginUseCase
+import chanwoo.cherhy.ktor.usecase.SignupUseCase
 import chanwoo.cherhy.ktor.util.EndPoint.CUSTOMER.DELETE_CUSTOMER
-import chanwoo.cherhy.ktor.util.EndPoint.CUSTOMER.GET_CUSTOMER
-import chanwoo.cherhy.ktor.util.EndPoint.CUSTOMER.GET_CUSTOMERS
+import chanwoo.cherhy.ktor.util.EndPoint.CUSTOMER.GET_ME
 import chanwoo.cherhy.ktor.util.EndPoint.CUSTOMER.LOGIN
+import chanwoo.cherhy.ktor.util.EndPoint.CUSTOMER.SIGN_UP
 import chanwoo.cherhy.ktor.util.EndPoint.CUSTOMER.UPDATE_CUSTOMER
-import chanwoo.cherhy.ktor.util.JwtManager
-import chanwoo.cherhy.ktor.util.PageRequest
 import chanwoo.cherhy.ktor.util.SecurityProperty.AUTHORITY
-import chanwoo.cherhy.ktor.util.id
+import chanwoo.cherhy.ktor.util.jwt
+import chanwoo.cherhy.ktor.util.userId
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.request.*
@@ -20,43 +20,33 @@ import org.koin.ktor.ext.inject
 
 fun Route.customer() {
     val customerService: CustomerService by inject()
-    val jwtManager: JwtManager by inject()
+    val loginUseCase: LoginUseCase by inject()
+    val signupUseCase: SignupUseCase by inject()
 
-    authenticate(AUTHORITY) {
-        get(LOGIN) {
-            val request = call.receive<LoginRequest>()
-            // TODO: insert validation logic
-            val jwt = jwtManager.createToken(0, request.username)
-            call.respond(jwt)
-        }
+    post(LOGIN) {
+        val request = call.receive<LoginRequest>()
+        val jwt = loginUseCase.execute(request)
+        call.respond(jwt)
+    }
+
+    post(SIGN_UP) {
+        val request = call.receive<CustomerRequest>()
+        signupUseCase.execute(request)
+        val jwt = loginUseCase.execute(request)
+        call.respond(jwt)
     }
 
     authenticate(AUTHORITY) {
-        get(GET_CUSTOMERS) {
-            val request = call.receive<PageRequest>()
-            customerService.getAll(request)
-        }
-    }
-
-    authenticate(AUTHORITY) {
-        get(GET_CUSTOMER) {
-            val customerId = call.id
+        get(GET_ME) {
+            val customerId = call.jwt.userId
             val customer = customerService.get(customerId)
             call.respond(customer)
         }
     }
 
     authenticate(AUTHORITY) {
-        post(CREATE_CUSTOMER) {
-            val request = call.receive<CustomerRequest>()
-            val customer = customerService.save(request)
-            call.respond(customer)
-        }
-    }
-
-    authenticate(AUTHORITY) {
         put(UPDATE_CUSTOMER) {
-            val customerId = call.id
+            val customerId = call.jwt.userId
             val request = call.receive<CustomerRequest>()
             customerService.update(customerId, request)
             call.respond("Customer updated")
@@ -65,7 +55,7 @@ fun Route.customer() {
 
     authenticate(AUTHORITY) {
         delete(DELETE_CUSTOMER) {
-            val customerId = call.id
+            val customerId = call.jwt.userId
             customerService.delete(customerId)
             call.respond("Customer deleted")
         }
