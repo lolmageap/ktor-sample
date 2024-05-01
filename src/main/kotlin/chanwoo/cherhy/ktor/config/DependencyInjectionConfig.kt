66220f8a7536db72f928b94016e60c5a5e1bee0f
lolmageap <ekxk1234@naver.com>
2024-05-01 17:12:25 +0900

    add jwt, login, signup to dependency injection

--- src/main/kotlin/chanwoo/cherhy/ktor/config/DependencyInjectionConfig.kt
+++ src/main/kotlin/chanwoo/cherhy/ktor/config/DependencyInjectionConfig.kt
@@ -4,6 +4,9 @@ import chanwoo.cherhy.ktor.domain.chat.*
 import chanwoo.cherhy.ktor.domain.customer.CustomerRepository
 import chanwoo.cherhy.ktor.domain.customer.CustomerRepositoryImpl
 import chanwoo.cherhy.ktor.domain.customer.CustomerService
+import chanwoo.cherhy.ktor.usecase.LoginUseCase
+import chanwoo.cherhy.ktor.usecase.SignupUseCase
+import chanwoo.cherhy.ktor.util.JwtManager
 import io.ktor.server.application.*
 import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
@@ -12,7 +15,10 @@ val dependencyInjectionModule = module {
     single<CustomerRepository> { CustomerRepositoryImpl() }
     single<ChatRepository> { ChatRepositoryImpl() }
     single<ChatRoomRepository> { ChatRoomRepositoryImpl() }
-    single { CustomerService(get(), get()) }
+    single { CustomerService(get()) }
+    single { JwtManager() }
+    single { LoginUseCase(get(), get(), get()) }
+    single { SignupUseCase(get(), get()) }
 }
 
 fun Application.configureDependencyInjection() {
