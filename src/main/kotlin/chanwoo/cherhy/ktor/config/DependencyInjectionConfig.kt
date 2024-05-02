7a6e1eab30f5540cb8a0bc87f8fb711cef822748
lolmageap <ekxk1234@naver.com>
2024-05-02 22:09:55 +0900

    add chat room domain in dependency injection

--- src/main/kotlin/chanwoo/cherhy/ktor/config/DependencyInjectionConfig.kt
+++ src/main/kotlin/chanwoo/cherhy/ktor/config/DependencyInjectionConfig.kt
@@ -4,6 +4,7 @@ import chanwoo.cherhy.ktor.domain.chat.*
 import chanwoo.cherhy.ktor.domain.customer.CustomerRepository
 import chanwoo.cherhy.ktor.domain.customer.CustomerRepositoryImpl
 import chanwoo.cherhy.ktor.domain.customer.CustomerService
+import chanwoo.cherhy.ktor.usecase.CreateChatRoomUseCase
 import chanwoo.cherhy.ktor.usecase.LoginUseCase
 import chanwoo.cherhy.ktor.usecase.SignupUseCase
 import chanwoo.cherhy.ktor.util.JwtManager
@@ -12,13 +13,18 @@ import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 
 val dependencyInjectionModule = module {
+    single { CreateChatRoomUseCase(get(), get(), get()) }
+    single { LoginUseCase(get(), get(), get()) }
+    single { SignupUseCase(get(), get()) }
+
     single<CustomerRepository> { CustomerRepositoryImpl() }
     single<ChatRepository> { ChatRepositoryImpl() }
     single<ChatRoomRepository> { ChatRoomRepositoryImpl() }
+
     single { CustomerService(get()) }
+    single { ChatRoomService(get()) }
+
     single { JwtManager() }
-    single { LoginUseCase(get(), get(), get()) }
-    single { SignupUseCase(get(), get()) }
 }
 
 fun Application.configureDependencyInjection() {
