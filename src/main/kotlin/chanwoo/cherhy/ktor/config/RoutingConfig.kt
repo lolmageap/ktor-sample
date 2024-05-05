6ca201151a321d7a966151dd14362b56264151f0
lolmageap <ekxk1234@naver.com>
2024-05-05 23:47:53 +0900

    fix health check url to constant

--- src/main/kotlin/chanwoo/cherhy/ktor/config/RoutingConfig.kt
+++ src/main/kotlin/chanwoo/cherhy/ktor/config/RoutingConfig.kt
@@ -2,6 +2,7 @@ package chanwoo.cherhy.ktor.config
 
 import chanwoo.cherhy.ktor.api.chat
 import chanwoo.cherhy.ktor.api.customer
+import chanwoo.cherhy.ktor.util.EndPoint.HOME
 import io.ktor.server.application.*
 import io.ktor.server.plugins.swagger.*
 import io.ktor.server.response.*
@@ -9,7 +10,7 @@ import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") { call.respondText("health check") }
+        get(HOME) { call.respondText("health check") }
         swaggerUI("/swagger")
         customer()
         chat()
