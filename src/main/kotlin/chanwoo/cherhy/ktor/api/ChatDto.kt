63e9827b2cd183a524028a1633f6e405586cd323
lolmageap <ekxk1234@naver.com>
2024-05-05 23:52:09 +0900

    create response dto to read chat room

--- src/main/kotlin/chanwoo/cherhy/ktor/api/ChatDto.kt
+++ src/main/kotlin/chanwoo/cherhy/ktor/api/ChatDto.kt
@@ -1,5 +1,7 @@
 package chanwoo.cherhy.ktor.api
 
+import chanwoo.cherhy.ktor.domain.chat.ChatRoom
+
 data class CreateChatRoomRequest(
     val targetCustomerIds: List<Long>,
     val name: String,
@@ -7,3 +9,25 @@ data class CreateChatRoomRequest(
     val password: String?,
     val maxUser: Int,
 )
+
+data class ChatRoomResponse(
+    val id: Long,
+    val name: String,
+    val maxUser: Int,
+    val currentUsers: Int,
+    val totalChats: Int,
+    val ownerId: Long,
+) {
+    companion object {
+        fun of(chatRoom: ChatRoom): ChatRoomResponse {
+            return ChatRoomResponse(
+                id = chatRoom.id.value,
+                name = chatRoom.name,
+                maxUser = chatRoom.maxUser,
+                currentUsers = chatRoom.currentUser,
+                totalChats = chatRoom.totalChats,
+                ownerId = chatRoom.owner.value,
+            )
+        }
+    }
+}
