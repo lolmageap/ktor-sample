0b004f0a9b0514b20d8050eb1cfab8dba02547d4
lolmageap <ekxk1234@naver.com>
2024-05-06 13:22:38 +0900

    add chatRoom router

--- src/main/kotlin/chanwoo/cherhy/ktor/config/RoutingConfig.kt
+++ src/main/kotlin/chanwoo/cherhy/ktor/config/RoutingConfig.kt
@@ -1,6 +1,7 @@
 package chanwoo.cherhy.ktor.config
 
 import chanwoo.cherhy.ktor.api.chat
+import chanwoo.cherhy.ktor.api.chatRoom
 import chanwoo.cherhy.ktor.api.customer
 import chanwoo.cherhy.ktor.util.EndPoint.HOME
 import io.ktor.server.application.*
@@ -14,5 +15,6 @@ fun Application.configureRouting() {
         swaggerUI("/swagger")
         customer()
         chat()
+        chatRoom()
     }
 }
