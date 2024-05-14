15ecb0ea6094b2a7df4f4233fba1a875a3926f5e
lolmageap <ekxk1234@naver.com>
2024-05-14 23:35:14 +0900

    fix constant to database

--- src/main/kotlin/chanwoo/cherhy/ktor/config/DatabaseConfig.kt
+++ src/main/kotlin/chanwoo/cherhy/ktor/config/DatabaseConfig.kt
@@ -5,17 +5,23 @@ import com.zaxxer.hikari.HikariDataSource
 import io.ktor.server.application.*
 import org.jetbrains.exposed.sql.Database
 import chanwoo.cherhy.ktor.util.ApplicationConfigUtils.getDataSource
+import chanwoo.cherhy.ktor.util.property.DataSourceProperty.DRIVER_CLASS_NAME
+import chanwoo.cherhy.ktor.util.property.DataSourceProperty.ISOLATION_LEVEL
+import chanwoo.cherhy.ktor.util.property.DataSourceProperty.MAX_POOL_SIZE
+import chanwoo.cherhy.ktor.util.property.DataSourceProperty.PASSWORD
+import chanwoo.cherhy.ktor.util.property.DataSourceProperty.USERNAME
+import chanwoo.cherhy.ktor.util.property.DataSourceProperty.URL
 
 fun Application.configureDatabase() {
     val hikari = HikariDataSource(
         HikariConfig().apply {
-            jdbcUrl = getDataSource("url")
-            username = getDataSource("username")
-            password = getDataSource("password")
-            driverClassName = getDataSource("driver-class-name")
-            maximumPoolSize = getDataSource("max-pool-size").toInt()
+            jdbcUrl = getDataSource(URL)
+            username = getDataSource(USERNAME)
+            password = getDataSource(PASSWORD)
+            driverClassName = getDataSource(DRIVER_CLASS_NAME)
+            maximumPoolSize = getDataSource(MAX_POOL_SIZE).toInt()
             isAutoCommit = false
-            transactionIsolation = getDataSource("isolation-level")
+            transactionIsolation = getDataSource(ISOLATION_LEVEL)
             validate()
         }
     )
