c0dd99c5c1575a6dd8da26246f26abe7c94d0440
lolmageap <ekxk1234@naver.com>
2024-05-16 22:16:34 +0900

    return video to client

--- src/main/kotlin/chanwoo/cherhy/ktor/api/VideoRouter.kt
+++ src/main/kotlin/chanwoo/cherhy/ktor/api/VideoRouter.kt
@@ -9,8 +9,11 @@ import chanwoo.cherhy.ktor.util.EndPoint.VIDEO.GET_VIDEOS
 import chanwoo.cherhy.ktor.util.EndPoint.VIDEO.UPLOAD_VIDEO
 import chanwoo.cherhy.ktor.util.extension.videoId
 import chanwoo.cherhy.ktor.util.property.SecurityProperty.AUTHORITY
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
 
@@ -23,10 +26,18 @@ fun Route.video() {
         }
     }
 
+    // TODO : 현재 동영상을 전체 가져오고 있지만 chunk 단위로 가져오는 방법으로 수정해야함
     authenticate(AUTHORITY) {
         get(GET_VIDEO) {
             val customer = call.jwt.customerId
             val videoId = call.videoId
+            val video = videoService.getVideo(customer, videoId)
+
+            call.respondBytes(
+                bytes = video,
+                contentType = call.request.contentType(),
+                status = HttpStatusCode.OK,
+            )
         }
     }
 
