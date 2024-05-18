f9373782247de33fbff5a5639048675fa9b40565
lolmageap <ekxk1234@naver.com>
2024-05-18 23:42:42 +0900

    refactor get video api

--- src/main/kotlin/chanwoo/cherhy/ktor/api/VideoRouter.kt
+++ src/main/kotlin/chanwoo/cherhy/ktor/api/VideoRouter.kt
@@ -7,45 +7,37 @@ import chanwoo.cherhy.ktor.util.EndPoint.VIDEO.UPLOAD_VIDEO
 import chanwoo.cherhy.ktor.util.extension.*
 import chanwoo.cherhy.ktor.util.property.SecurityProperty.AUTHORITY
 import io.ktor.http.*
+import io.ktor.http.ContentType.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.netty.handler.codec.http.HttpHeaders.Names.*
 import org.koin.ktor.ext.inject
+import java.lang.Byte.BYTES
 
 fun Route.video() {
     val videoService: VideoService by inject()
 
     authenticate(AUTHORITY) {
         get(GET_VIDEOS) {
-            val customer = call.jwt.customerId
+            val customerId = call.jwt.customerId
+            val videos = videoService.getVideos(customerId)
+            call.respond(videos)
         }
     }
 
-    // TODO : 현재 동영상을 전체 가져오고 있지만 chunk 단위로 가져오는 방법으로 수정해야함
     authenticate(AUTHORITY) {
         get(GET_VIDEO) {
-            val customer = call.jwt.customerId
+            val customerId = call.jwt.customerId
             val videoId = call.videoId
             val lastVideoByte = call.lastVideoByte
-            val video = videoService.getVideo(customer, videoId, lastVideoByte)
+            val video = videoService.getVideo(customerId, videoId, lastVideoByte)
 
-//            call.response.header("Content-Type", "video/mp4")
-//            call.response.header("Accept-Ranges", "bytes")
-//            call.response.header(
-//                "Content-Length",
-//                calculateContentLengthHeader(parsedRange, video.size)
-//            )
-//            call.response.header(
-//                "Content-Range",
-//                constructContentRangeHeader(parsedRange, chunkWithMetadata.metadata.size)
-//            )
-//
-//            call.respondBytes(
-//                status = HttpStatusCode.PartialContent,
-//                bytes = chunkWithMetadata.chunk,
-//            )
+            call.response.header(ACCEPT_RANGES, BYTES)
+            call.response.header(CONTENT_LENGTH, video.size)
+            call.response.header(CONTENT_RANGE, video.size)
+            call.respondBytes(Video.MP4, HttpStatusCode.PartialContent) { video }
         }
     }
 
