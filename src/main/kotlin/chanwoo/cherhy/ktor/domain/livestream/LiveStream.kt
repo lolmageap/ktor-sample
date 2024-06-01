f3e08f26068221bb460f34359ffbb3b9bc4ae6f4
lolmageap <ekxk1234@naver.com>
2024-06-01 22:58:07 +0900

    refactor live stream id type to value class

--- src/main/kotlin/chanwoo/cherhy/ktor/domain/livestream/LiveStream.kt
+++ src/main/kotlin/chanwoo/cherhy/ktor/domain/livestream/LiveStream.kt
@@ -1,23 +1,37 @@
 package chanwoo.cherhy.ktor.domain.livestream
 
-import chanwoo.cherhy.ktor.domain.customer.CustomerId
 import chanwoo.cherhy.ktor.domain.customer.Customers
 import chanwoo.cherhy.ktor.util.model.BaseEntity
 import chanwoo.cherhy.ktor.util.model.BaseEntityClass
 import chanwoo.cherhy.ktor.util.model.BaseLongIdTable
 import org.jetbrains.exposed.dao.id.EntityID
-import org.jetbrains.exposed.sql.Column
-
-typealias LiveStreamId = Long
 
 object LiveStreams: BaseLongIdTable("live_stream", "id") {
-    val owner: Column<EntityID<CustomerId>> = reference("customer_id", Customers)
+    val owner = reference("customer_id", Customers)
 }
 
-class LiveStream(id: EntityID<LiveStreamId>): BaseEntity(
-    id = id,
+class LiveStream(
+    id: EntityID<LiveStreamId>,
+): BaseEntity(
+    id = id.unwrap(),
     table = LiveStreams,
 ) {
     var owner by LiveStreams.owner
     companion object: BaseEntityClass<LiveStream>(LiveStreams)
 }
+
+@JvmInline
+value class LiveStreamId(
+    val value: Long,
+): Comparable<LiveStreamId> {
+    override fun compareTo(
+        other: LiveStreamId,
+    ) = value.compareTo(other.value)
+    companion object {
+        fun of(
+            value: Long,
+        ) = LiveStreamId(value)
+    }
+}
+
+private fun EntityID<LiveStreamId>.unwrap() = EntityID(value.value, LiveStreams)
